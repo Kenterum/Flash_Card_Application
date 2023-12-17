17c6e7ed422e8851f027f9775ebe7b86f8b6f024
Kenterum <suleymammadov@gmail.com>
2023-12-17 23:20:00 +0400

    Enhance ContactPage with Validation and Feedback: - Added CSS for styling and user feedback messages. - Integrated email validation and form field checks before submission. - Implemented loading state to disable form while sending and provide user feedback with a spinner. - Added error and success state handling to display messages to the user.

--- flashcard-app/src/components/ContactPage/ContactPage.jsx
+++ flashcard-app/src/components/ContactPage/ContactPage.jsx
@@ -1,36 +1,62 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './ContactPage.css'
 
 const ContactPage = () => {
-  // State variables for the form inputs
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
+  const validateEmail = (email) => {
+    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return re.test(String(email).toLowerCase());
+  };
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setError('');
+    setSuccess('');
+
+    if (!email.trim() || !subject.trim() || !message.trim()) {
+      setError('Please fill in all fields.');
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address.');
+      return;
+    }
+
+    setLoading(true);
     try {
       // Post the data to the server
-      const result = await axios.post('http://localhost:3000/messages', {
+      await axios.post('http://localhost:3000/messages', {
         email,
         subject,
         message
       });
-      // Log the result or handle the response here
-      console.log(result.data);
-      // Optionally clear the form or give feedback to the user
+      // Handle the response here (e.g., clear the form, notify the user)
+      setSuccess('Message sent successfully!');
       setEmail('');
       setSubject('');
       setMessage('');
     } catch (error) {
       // Handle any errors here, like showing an error message to the user
       console.error("There was an error sending the message:", error);
+      setError('There was an error sending your message.'); // Notify the user of the error
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div>
       <h1>Contact Us</h1>
+      {error && <div className="error">{error}</div>}
+      {success && <div className="success">{success}</div>}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email:</label>
@@ -39,6 +65,7 @@ const ContactPage = () => {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            disabled={loading}
           />
         </div>
         <div>
@@ -48,6 +75,7 @@ const ContactPage = () => {
             type="text"
             value={subject}
             onChange={(e) => setSubject(e.target.value)}
+            disabled={loading}
           />
         </div>
         <div>
@@ -56,9 +84,12 @@ const ContactPage = () => {
             id="message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
+            disabled={loading}
           />
         </div>
-        <button type="submit">Send Message</button>
+        <button type="submit" disabled={loading}>
+  {loading ? <div className="loading-spinner"></div> : 'Send Message'}
+</button>
       </form>
     </div>
   );
