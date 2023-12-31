3cafa33b086d9e707b8677caf88bcb415ea4ec90
Kenterum <suleymammadov@gmail.com>
2024-01-01 01:20:00 +0400

    Enhance FlashCard Component: - Added conditional class for flipping animation. - Implemented card editing and deletion functionality with buttons. - Integrated 'Flashcard.css' for custom styling of the card's front and back. - Added status indicator and content areas for the card's details.

--- flashcard-app/src/components/Flashcard/Flashcard.jsx
+++ flashcard-app/src/components/Flashcard/Flashcard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import './Flashcard.css';
 
-const FlashCard = ({ card }) => {
+const FlashCard = ({ card, onEdit, onDelete }) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const flipCard = () => {
@@ -8,12 +9,18 @@ const FlashCard = ({ card }) => {
   };
 
   return (
-    <div onClick={flipCard}>
-      {isFlipped ? (
-        <div>{/* Back of the card content */}</div>
-      ) : (
-        <div>{/* Front of the card content */}</div>
-      )}
+    <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={flipCard}>
+      <div className="card-inner">
+        <div className="card-front">
+          <div className={`status ${card.status.replace(/\s+/g, '')}`}>{card.status}</div>
+          <div className="content">{card.front}</div>
+        </div>
+        <div className="card-back">
+          {card.back}
+          <button className="edit-btn" onClick={(e) => { e.stopPropagation(); onEdit(); }}>Edit</button>
+          <button className="delete-btn" onClick={(e) => { e.stopPropagation(); onDelete(); }}>Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
